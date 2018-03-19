f3103ce646b1fe610a2839514b2ba172e3883eb6
Gustavo S. Rodrigues <gustavo.beavis@gmail.com>
2018-03-19 00:41:25 -0300

    change mocha to kill after test

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const path = require('path');
 const app = express();
 
 consign({
-    cwd: path.join(__dirname, ''),
-    extensions: [ '.js' ],
-    verbose: true
+  cwd: path.join(__dirname, ''),
+  extensions: [ '.js' ],
+  verbose: true
 })
-    .include('./config/config.js')
-    .then('./domain/datasource.js')
-    .then('./domain/repositories/')
-    .then('./http/middlewares/')
-    .then('./http/controllers')
-    .then('./http/routes/')
-    .then('boot.js')
-    .into(app);
+  .include('./config/config.js')
+  .then('./domain/datasource.js')
+  .then('./domain/repositories/')
+  .then('./http/middlewares/')
+  .then('./http/controllers')
+  .then('./http/routes/')
+  .then('boot.js')
+  .into(app);
 
 module.exports = app;
