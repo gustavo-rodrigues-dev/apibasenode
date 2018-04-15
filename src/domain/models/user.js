2194b7152968af98e226691fbdd8f57ece968f02
Gustavo S. Rodrigues <gustavo.beavis@gmail.com>
2018-04-15 02:03:06 -0300

    lint models

--- src/domain/models/user.js
+++ src/domain/models/user.js
@@ -1,7 +1,6 @@
-import {Model} from 'sequelize';
 import PasswordFactory from '../../lib/auth/password.factory'
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const user = sequelize.define('user', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
@@ -10,15 +9,15 @@ module.exports = function(sequelize, DataTypes) {
     freezeTableName: true,
     tableName: 'user',
     hooks: {
-        beforeCreate: user => {
-            const newUser = user;
-            newUser.password = PasswordFactory.create(newUser.password);
-        }
+      beforeCreate: user => {
+        const newUser = user
+        newUser.password = PasswordFactory.create(newUser.password)
+      }
     }
-  });
+  })
 
-  user.associate = (models) => {};
-  user.isPassword = (encPass, pass) => PasswordFactory.compare(pass, encPass);
+  user.associate = (models) => {}
+  user.isPassword = (encPass, pass) => PasswordFactory.compare(pass, encPass)
 
-  return user;
-};
+  return user
+}
