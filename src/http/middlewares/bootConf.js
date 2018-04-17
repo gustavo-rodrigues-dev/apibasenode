8ca85daee60de1647aafc1aa76e8652dc32588c5
Gustavo S. Rodrigues <gustavo.beavis@gmail.com>
2018-04-17 00:07:57 -0300

    add app boot port to observe config port

--- src/http/middlewares/bootConf.js
+++ src/http/middlewares/bootConf.js
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser'
 
 module.exports = app => {
-  app.set('port', 3000)
+  app.set('port', app.config.port)
   app.set('json spaces', 4)
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json())
