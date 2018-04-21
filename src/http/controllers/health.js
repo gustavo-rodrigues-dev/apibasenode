6d1ac424e994e60caf8af56d9dae6ae768162714
Gustavo S. Rodrigues <gustavo.beavis@gmail.com>
2018-04-20 22:35:44 -0300

    removing unnecessary behavior on route health

--- src/http/controllers/health.js
+++ src/http/controllers/health.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   class HealthController {
-    static liveness (req, res) {
+    static liveness (req, res, next) {
       app.domain.datasource.sequelize.authenticate()
         .then(() => {
           app.logger.info('Connection services has been established successfully.')
@@ -9,15 +9,7 @@ module.exports = (app) => {
             message: 'success on conect services'
           })
         })
-        .catch(error => {
-          app.logger.error('Unable to connect to the database:', error)
-          return res
-            .sendStatus(500)
-            .json({
-              status: 500,
-              message: 'error o connect services'
-            })
-        })
+        .catch(error => next(error))
     }
   }
 
